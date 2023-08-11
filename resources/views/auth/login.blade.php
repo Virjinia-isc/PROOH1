c08380aa307766496849f10e57b85a6732feb6c1
VickiM6 <117034456+VickiM6@users.noreply.github.com>
2023-08-11 13:39:30 -0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,8 +12,10 @@
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     
     <link rel = "stylesheet"href= "{{asset ('assets/estilos.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-</head>
+
+  </head>
 
 <body>
 <section class="h-100 gradient-form" style="background-color: #eee;">
@@ -41,7 +43,12 @@
 
                   <div class="form-outline mb-4">
                     <label class="form-label" for="form2Example22">Contraseña</label>
-                    <input type="password" name="pasword" id="form2Example22" class="form-control" required/>
+                    <div class="input-group position-relative">
+                      <input type="password" name="password" id="form2Example22" class="form-control" minlength="8" required/>
+                      <button class="btn btn-outline-secondary password-toggle-icon" type="button" id="togglePassword">
+                        <i class="fas fa-eye"></i>
+                      </button>
+                    </div>
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
@@ -51,9 +58,16 @@
 
                   <div class="d-flex align-items-center justify-content-center pb-4">
                     <p class="mb-0 me-2">¿No tienes cuenta?</p>
-                    <a href="{{route('register')}}" class="btn btn-outline-danger">Registrarse</a>
-                    
+                    <a href="{{route('register')}}" class="btn btn-outline-danger">Registrarse</a>  
                   </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                      <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/PROOH.MX?mibextid=ZbWKwL" style="margin-right: 10px;">
+                      <img src="https://cdn-icons-png.flaticon.com/128/2504/2504903.png" style="width: 20px;" alt="facebook"></a>
+                      <img src="https://cdn-icons-png.flaticon.com/128/5968/5968534.png" style="width: 20px;" alt="email"></a>
+                  </div>
+                    
+                    
 
                 </form>
 
@@ -79,6 +93,28 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      const togglePassword = document.querySelector("#togglePassword");
+      const passwordInput = document.querySelector("#form2Example22");
+    
+      togglePassword.addEventListener("click", function() {
+        const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+        passwordInput.setAttribute("type", type);
+    
+        // Cambiar el ícono del botón
+        const icon = this.querySelector("i");
+        if (icon.classList.contains("fa-eye")) {
+          icon.classList.remove("fa-eye");
+          icon.classList.add("fa-eye-slash");
+        } else {
+          icon.classList.remove("fa-eye-slash");
+          icon.classList.add("fa-eye");
+        }
+      });
+    });
   </script>
 </body>
 
